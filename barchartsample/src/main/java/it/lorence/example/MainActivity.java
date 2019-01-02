5f4d1eab6eb7dbd1674e092c7ef2321416b7e242
lorence <lorence@enclave.vn>
2019-01-02 17:47:28 +0700

    Done

--- barchartsample/src/main/java/it/lorence/example/MainActivity.java
+++ barchartsample/src/main/java/it/lorence/example/MainActivity.java
@@ -1,10 +1,8 @@
 package it.lorence.example;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.Random;
 
@@ -13,17 +11,11 @@ import it.lorence.barchart.BarChartModel;
 
 public class MainActivity extends AppCompatActivity {
     private BarChart barChartVertical;
-    private EditText etAddViewAt;
-    private EditText etRemoveViewAt;
-    private EditText etUpdateViewAt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         barChartVertical = (BarChart) findViewById(R.id.bar_chart_vertical);
-        etAddViewAt = (EditText) findViewById(R.id.et_add_view_at);
-        etRemoveViewAt = (EditText) findViewById(R.id.et_remove_bar_at);
-        etUpdateViewAt = (EditText) findViewById(R.id.et_update_bar_at);
         for (int i = 0; i < 3; i++) {
             BarChartModel barChartModel = new BarChartModel();
             barChartModel.setBarValue(new Random().nextInt(100));
@@ -37,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 "JavaScript",
                 "C"
         };
+
         int dataColor[] = new int[] {
                 Color.parseColor("#00BCD4"),
                 Color.parseColor("#3F51B5"),
@@ -47,68 +40,12 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 4; i++) {
             BarChartModel barChartModel = new BarChartModel();
             barChartModel.setBarValue(new Random().nextInt(100));
-            barChartModel.setBarColor(dataColor[i]);
+            Random rnd = new Random();
+            barChartModel.setBarColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
             barChartModel.setBarTag(null);
-            barChartModel.setBarText(data[i]);
+            barChartModel.setBarText("Hello World");
+            barChartVertical.addBar(barChartModel);
         }
-
-
-        findViewById(R.id.tv_hello).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("Clicked::");
-                BarChartModel barChartModel = new BarChartModel();
-                barChartModel.setBarValue(new Random().nextInt(100));
-                Random rnd = new Random();
-                barChartModel.setBarColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
-                barChartModel.setBarTag(null);
-                barChartVertical.addBar(barChartModel);
-            }
-        });
-
-        findViewById(R.id.tv_hello_at).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("Clicked::");
-                BarChartModel barChartModel = new BarChartModel();
-                barChartModel.setBarValue(new Random().nextInt(100));
-                Random rnd = new Random();
-                barChartModel.setBarColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
-                barChartModel.setBarTag(null);
-                barChartVertical.addBar(Integer.parseInt(etAddViewAt.getText().toString()),barChartModel);
-            }
-        });
-        findViewById(R.id.tv_clear).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        barChartVertical.clearAll();
-                    }
-                });
-
-        findViewById(R.id.tv_remove_bar_at).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                barChartVertical.removeBar(Integer.parseInt(etRemoveViewAt.getText().toString()));
-            }});
-
-        findViewById(R.id.tv_update_bar_at).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                BarChartModel barChartModel = new BarChartModel();
-                barChartModel.setBarValue(new Random().nextInt(100));
-                Random rnd = new Random();
-                barChartModel.setBarColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
-                barChartModel.setBarTag(null);
-                barChartVertical.updateBar(Integer.parseInt(etUpdateViewAt.getText().toString()),barChartModel);
-            }});
-
-        barChartVertical.setOnBarClickListener(new BarChart.OnBarClickListener() {
-            @Override
-            public void onBarClick(BarChartModel barChartModel) {
-                barChartVertical.removeBar(barChartModel);
-            }
-        });
 
     }
 }
